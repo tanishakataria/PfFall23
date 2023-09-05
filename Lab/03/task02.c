e651a2ed58d48dd967c8b344cad8e1766cbdf727
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2023-09-05 10:49:39 +0500

    Update and rename readme.md to task02.c

---
+++ Lab/03/task02.c
@@ -0,0 +1,13 @@
+QUESTION;2
+#include <stdio.h>
+  int main () {
+        int rad=5;
+        int height=20;
+        int pi=3.142;
+    printf("FORMULA TO FIND VOLUME OF BALL CONTAINER /n");
+    int basearea= pi* rad * rad;
+    int volume = basearea * height;
+     printf("The volume is: %d/n", volume);
+   return 0;
+  }//end main
+
