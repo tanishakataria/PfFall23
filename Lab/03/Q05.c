4d5728d10b32d5576e4a8116bbc617fc24400ac8
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2023-09-06 14:46:08 +0500

    Create Q05.c

---
+++ Lab/03/Q05.c
@@ -0,0 +1,16 @@
+QUESTION;5
+  // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int south=10;
+    int west=10;
+    int south2=5;
+    int north=15;
+   int totaldistance;
+   totaldistance= south+west+south2+north;
+   int totalfuel;
+   totalfuel=2*totaldistance;
+   printf("total distance is %d",totaldistance);
+   printf("total fuel is %d",totalfuel);
+}
