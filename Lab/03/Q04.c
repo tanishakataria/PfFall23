593ac87732ac1b23c94cb3ba9bf413551c43dd0c
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2023-09-06 14:23:20 +0500

    Create Q04.c

---
+++ Lab/03/Q04.c
@@ -0,0 +1,12 @@
+QUESTION:4
+  #include<stdio.h>
+#include<math.h>
+int main()
+{
+    int originalprice,discountprice;
+    originalprice=1500;
+    discountprice=originalprice-(originalprice*0.2);
+    printf("the discountedprice is %d",discountprice);
+    
+    
+}
