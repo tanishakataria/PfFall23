84ea9552d82e413da6d020e8c2cd37c9a9d30aec
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2023-09-12 22:03:54 +0500

    Create QUESTION02.c

---
+++ Lab/04/QUESTION02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+ int main(){
+         int n;
+        // Input the value of n 
+         printf("Enter the value of n:");
+         scanf("%d",&n);
+        int i=1,j=1;
+   for (i=0;i=n;i++){
+     for(j=0;j=i;j++){
+     		printf("*");
+	}
+	printf("\n");
+    }
+     
+} 
