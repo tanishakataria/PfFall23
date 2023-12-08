0481148f70778bb136a410efcc519509b93236f7
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2023-12-08 15:08:14 +0500

    Update questiom3.c

--- ASSIGEMENT/A3/questiom3.c
+++ ASSIGEMENT/A3/questiom3.c
@@ -1,1 +1,19 @@
+/**
+ * Programmer: tanisha
+ * Desc: Sum of digits
+ * Date: 10/10/2023
+ * Roll-No: 23K-0047
+ */ 
+#include <stdio.h>
 
+int main() {
+	int n, sum = 0;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	while (n != 0) {
+		sum += n % 10;
+		n = n / 10;
+	}
+	printf("Sum of Digits is: %d", sum);
+	return 1;
+}
